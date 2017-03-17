0ea4f0a673a74f8d26f8e4306214d4c1d10518da
RichardRhyan <richard.rhyan@gmail.com>
2017-03-17 15:50:15 -0500

    Initial create

---
+++ com/rhyan/eightPointDirection/Direction.java
@@ -0,0 +1,92 @@
+package com.rhyan.eightPointDirection;
+
+public enum Direction {
+	NORTH ("North"),
+	NORTHEAST ("Northeast"),
+	EAST ("East"),
+	SOUTHEAST ("Southeast"),
+	SOUTH ("South"),
+	SOUTHWEST ("Southwest"),
+	WEST ("West"),
+	NORTHWEST ("Northwest");
+	
+	private String direction;
+	
+	private Direction(String direction) {
+		this.direction = direction;
+	}
+	
+	@Override
+	public String toString() {
+		return direction;
+	}
+	
+	/**
+	 * Returns the Direction immediately clockwise of this Direction.
+	 * @return Direction
+	 */
+	public Direction clockwise() {
+		Direction d;
+		try {
+			d = Direction.values()[this.ordinal() + 1];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			d = NORTH;
+		}
+		return d;
+	}
+	
+	/**
+	 * Returns the Direction a number facings clockwise of this Direction.
+	 * @return Direction
+	 */
+	public Direction clockwise(int steps) {
+		Direction d = this;
+		for (int i = 0; i < steps; i++)
+			d = d.clockwise();
+		
+		return d;
+	}
+	
+	/**
+	 * Returns the Direction immediately counterclockwise of this Direction.
+	 * @return Direction
+	 */
+	public Direction counterClockwise() {
+		Direction d;
+		try {
+			d = Direction.values()[this.ordinal() - 1];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			d = NORTHWEST;
+		}
+		return d;
+	}
+	
+	/**
+	 * Returns the Direction a number facings counterclockwise of this Direction.
+	 * @return Direction
+	 */
+	public Direction counterClockwise(int steps) {
+		Direction d = this;
+		for (int i = 0; i < steps; i++)
+			d = d.counterClockwise();
+		
+		return d;
+	}
+	
+	/**
+	 * Returns the Direction opposite of this Direction.
+	 * @return Direction
+	 */
+	public Direction oppositeDirection() {
+		return counterClockwise(4);
+	}
+	
+	/**
+	 * Returns True if North, East, South, or West. 
+	 * @return boolean
+	 */
+	public boolean isCardinalDirection() {
+		return (this.ordinal()+1) % 2 != 0;
+	}
+}
+
